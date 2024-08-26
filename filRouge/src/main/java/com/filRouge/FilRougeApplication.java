075044971fe6d01df06efd4b9e52e7e47b596448
zineb zarda <zinebzarda1@gmail.com>
2024-08-26 10:30:00 +0100

    first commit

---
+++ filRouge/src/main/java/com/filRouge/FilRougeApplication.java
@@ -0,0 +1,13 @@
+package com.filRouge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilRougeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilRougeApplication.class, args);
+	}
+
+}
