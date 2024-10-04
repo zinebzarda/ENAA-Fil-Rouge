27e0d01856546713c5b91178a85051d6481de175
zineb zarda <zinebzarda1@gmail.com>
2024-10-04 22:49:24 +0100

    logout

--- FrontEnd/JobRabbit/src/app/logout/logout.component.ts
+++ FrontEnd/JobRabbit/src/app/logout/logout.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
   styleUrls: ['./logout.component.css']
 })
-export class LogoutComponent {
+export class LogoutComponent implements AfterViewInit{
+
+  constructor(private authService : AuthService) {}
+
+  ngAfterViewInit() : void {
+    this.authService.logout()
+  }
 
 }
