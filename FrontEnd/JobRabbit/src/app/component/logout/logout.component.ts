cff2f65b51e08aa4f76550320f165020100b2e36
zineb zarda <zinebzarda1@gmail.com>
2024-10-08 12:14:04 +0100

    modification de logout et login

--- FrontEnd/JobRabbit/src/app/component/logout/logout.component.ts
+++ FrontEnd/JobRabbit/src/app/component/logout/logout.component.ts
@@ -1,5 +1,5 @@
 import {AfterViewInit, Component} from '@angular/core';
-import {AuthService} from "../../services/auth.service";
+import {AuthService} from "../../core/services/auth.service";
 
 @Component({
   selector: 'app-logout',
